16346fa24468a66adc9b9ffab7b011f829a076a1
John Kerfoot <kerfoot@marine.rutgers.edu>
2016-11-28 13:42:59 -0500

    added to repo

---
+++ backend/batch/bashrc-env.sh
@@ -0,0 +1,11 @@
+export OOI_ERDDAP_ASYNC_HOME=/var/local/erddap/erddap-storage/bin/erddap-async;
+export OOI_ERDDAP_DATA_HOME=/var/local/erddap/erddap-storage;
+export OOI_ERDDAP_UFRAME_NC_ROOT=/opendap_export/async_results;
+export OOI_ERDDAP_BACKEND_BASE_URL=http://192.168.163.231;
+export OOI_ERDDAP_FRONTEND_BASE_URL=http://192.168.161.231;
+
+export UFRAME_BASE_URL=http://uframe-test.ooi.rutgers.edu;
+export UFRAME_USER=erddap-user;
+
+export PYTHONPATH=/var/local/erddap/erddap-storage/bin/erddap-async:/var/local/erddap/erddap-storage/bin/uframe-api
+
